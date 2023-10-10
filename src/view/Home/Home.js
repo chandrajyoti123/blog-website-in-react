efa2737a3189827be4fee6196491cffb90e83326
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-10-10 19:50:12 +0530

    header added

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -18,8 +18,39 @@ export default function Home() {
     }
     return (
         <>
+        <div className='header'>
+            <div className='navigationbar'>
+                <div className='logo'>
+                    <span className='navbarlogo'>bloggie</span>
 
-     <FirstBlogCard/>
+                </div>
+                <div className='navlinkbar'>
+                <div className='navlink'>Home</div>
+                <div className='navlink'>Landings</div>
+                <div className='navlink'>Pages</div>
+                <div className='navlink'>Docs</div>
+                <div className='navlink'>Help</div>
+                </div>
+                <div className='btncontainer'>
+                    <button type='button' className='navbarbtn'>get it now</button>
+                </div>
+
+            </div>
+            <div className='navbarbody'>
+                <div className='navbar-main-heading'>Our BlogRoom</div>
+                <input type='text' placeholder='Search Blog' className='searchbar'/>
+
+            </div>
+
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='waveofheader'><path fill="#fff" fill-opacity="1" d="M0,256L120,234.7C240,213,480,171,720,170.7C960,171,1200,213,1320,234.7L1440,256L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg> 
+        </div>
+
+            <div className='body-of-website'>
+           <div className='section-three'> <FirstBlogCard/></div>
+           <div className='heading'>
+            Popular Blogs
+           </div>
             <div className='section-one-con'>
                 <div className='section-one-left'>
                     <span onClick={() => {
@@ -46,6 +77,9 @@ export default function Home() {
                     }}> <FontAwesomeIcon icon={faAngleRight} className='scroll-angle' /></span>
                 </div>
             </div>
+            <div className='heading'>
+            Recent Blogs
+           </div>
             <div className='section-tow-con'>
 
                 {
@@ -61,6 +95,12 @@ export default function Home() {
 
 
             </div>
+            </div>
+            <div className='footer'>
+                Made with love by chadrajyoti
+
+            </div>
+             
         </>
     )
 }
